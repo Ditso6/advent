a9a92c0e66b217a6ba68ac1b0245fe8d6c5853cc
Dieter Sauvillers <dieter.sauvillers@gmail.com>
2022-12-07 10:46:47 +0100

    day6

--- 2022/day6/day6p2.py
+++ 2022/day6/day6p2.py
@@ -7,7 +7,6 @@ position = 0
 for i in range(0, len(stream)-14, 1):
     marker = set(stream[i:i+14])
     if len(marker) == 14:
-        print(marker)
         position = i+14
         break
     else:
