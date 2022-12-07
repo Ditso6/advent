313e363750125a96b5179bed66e5a63d5cebaf06
Dieter Sauvillers <dieter.sauvillers@gmail.com>
2022-12-07 10:52:52 +0100

    day6

--- 2022/day6/day6p1.py
+++ 2022/day6/day6p1.py
@@ -5,8 +5,7 @@ stream = file.readline().rstrip()
 
 position = 0
 for i in range(0, len(stream)-4, 1):
-    marker = set(stream[i:i+4])
-    if len(marker) == 4:
+    if len(set(stream[i:i+4])) == 4:
         position = i+4
         break
     else:
--- 2022/day6/day6p2.py
+++ 2022/day6/day6p2.py
@@ -5,8 +5,7 @@ stream = file.readline().rstrip()
 
 position = 0
 for i in range(0, len(stream)-14, 1):
-    marker = set(stream[i:i+14])
-    if len(marker) == 14:
+    if len(set(stream[i:i+14])) == 14:
         position = i+14
         break
     else:
